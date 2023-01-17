61da4631c73bcee1567ea122a3cdc6d07df18b72
Ezequiel <epuyosdacosta@gmail.com>
2023-01-17 13:35:28 +0100

    Change names and create index

---
+++ app/controllers/pokemons_controller.rb
@@ -0,0 +1,22 @@
+class PokemonsController < ApplicationController
+  def index
+    if params[:search_term].present?
+      begin
+        @pokemon = PokeapiClient.new.get_pokemon(params[:search_term])
+      rescue RestClient::NotFound
+        render template: "errors/404", status: 404
+      end
+    else
+      query = request.query_string
+      @pokemons = PokeapiClient.new.all_pokemon(query)
+      @next = PokeapiClient.new.next(query)
+      @previous = PokeapiClient.new.previous(query)
+    end
+  end
+
+  def show
+    @pokemon = PokeapiClient.new.get_pokemon(params[:id])
+    @pokemon_details = PokeapiClient.new.get_pokemon_details(@pokemon.id)
+    @pokemon_evolves = PokeapiClient.new.get_pokemon_evolves(@pokemon.id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  resources :restaurants, only: [:index, :show]
+  resources :pokemons, only: [:index, :show]
 end
