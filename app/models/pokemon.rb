330e60db2b5ef2ba0d96c21d4c6f832f2163932e
Ezequiel <epuyosdacosta@gmail.com>
2023-01-17 13:25:21 +0100

    Model created

---
+++ app/models/pokemon.rb
@@ -0,0 +1,9 @@
+class Pokemon < ApplicationRecord
+  attr_accessor :name, :weight, :id, :photos, :types, :abilities, :effect_entries, :chains
+
+  def initialize(name:, weight:, id:)
+    @name = name
+    @weight = weight
+    @id = id
+  end
+end
