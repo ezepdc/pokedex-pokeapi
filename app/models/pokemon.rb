5f3d2bee3d2c8ec4c3269fa997b8f9dc23c5d969
Ezequiel <epuyosdacosta@gmail.com>
2023-01-17 17:45:49 +0100

    banner style

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -2,7 +2,7 @@ class Pokemon
   attr_accessor :name, :weight, :id, :photos, :types, :abilities, :effect_entries, :chains
 
   def initialize(name:, weight:, id:)
-    @name = name.capitalize
+    @name = name
     @weight = weight
     @id = id
   end
