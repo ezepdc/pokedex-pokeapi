0d576f7bc1142feb7b0a1befef626ea4431df202
Ezequiel <epuyosdacosta@gmail.com>
2023-01-18 10:19:30 +0100

    refactor controller

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,17 +1,16 @@
 class PokemonsController < ApplicationController
   def index
+    pokeapi = PokeapiClient.new
     if params[:search_term].present?
-      begin
-        @pokemon = PokeapiClient.new.get_pokemon(params[:search_term].downcase)
-      rescue RestClient::NotFound
-        render template: "errors/404", status: 404
-      end
+      @pokemon = pokeapi.get_pokemon(params[:search_term].downcase)
     else
       query = request.query_string
-      @pokemons = PokeapiClient.new.all_pokemon(query)
-      @next = PokeapiClient.new.next(query)
-      @previous = PokeapiClient.new.previous(query)
+      @pokemons = pokeapi.all_pokemon(query)
+      @next = pokeapi.next(query)
+      @previous = pokeapi.previous(query)
     end
+  rescue RestClient::NotFound
+    render template: "errors/404", status: 404
   end
 
   def show
