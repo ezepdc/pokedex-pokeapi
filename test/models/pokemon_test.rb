401586ccd252c7c2e7ec2a61978eaf7f5014e88e
Ezequiel <epuyosdacosta@gmail.com>
2023-01-18 17:55:06 +0100

    Pokemon model test

--- test/models/pokemon_test.rb
+++ test/models/pokemon_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 class PokemonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @pokemon = Pokemon.new(name: "Pikachu", weight: 6.0, id: 25)
+  end
+
+  def test_name
+    assert_equal "Pikachu", @pokemon.name
+  end
+
+  def test_weight
+    assert_equal 6.0, @pokemon.weight
+  end
+
+  def test_id
+    assert_equal 25, @pokemon.id
+  end
+
+  def test_photos
+    assert_nil @pokemon.photos
+  end
+
+  def test_types
+    assert_nil @pokemon.types
+  end
+
+  def test_abilities
+    assert_nil @pokemon.abilities
+  end
+
+  def test_effect
+    assert_nil @pokemon.effect
+  end
+
+  def test_evolves
+    assert_nil @pokemon.evolves
+  end
 end
