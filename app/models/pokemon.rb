20060aad25bebd9c4f8a5976be78d1b07a1be12d
ezepdc <epuyosdacosta@gmail.com>
2023-08-08 18:59:59 +0200

    Change initialize to ActiveModel

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,9 +1,4 @@
 class Pokemon
+  include ActiveModel::Model
   attr_accessor :name, :weight, :api_id, :photos, :types, :abilities, :effect, :evolves
-
-  def initialize(name:, weight:, api_id:)
-    @name = name
-    @weight = weight
-    @api_id = api_id
-  end
 end
